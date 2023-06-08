cb05f414a3a9458fecc20a83eefd25b3527459fa
channaveer <channaveerpatil999@gmail.com>
2023-06-09 00:02:02 +0530

    edit delete button added

--- src/component/Addinput.jsx
+++ src/component/Addinput.jsx
@@ -33,43 +33,43 @@ const Addinput = () => {
   console.log('user', user);
 
   return (
-    <form
-      className='flex flex-col m-auto w-1/3 gap-y-5'
-      onSubmit={handleSubmit}
-    >
-      <input
-        type='text'
-        placeholder='Add email'
-        className='border-4 p-2 mr-2'
-        name='email'
-        value={userInput.email}
-        onChange={handleChange}
-      />
-      <input
-        type='password'
-        placeholder='Add password'
-        className='border-4 p-2 mr-2'
-        name='password'
-        value={userInput.password}
-        onChange={handleChange}
-      />
-      <input
-        type='text'
-        placeholder='Add phone'
-        className='border-4 p-2 mr-2'
-        name='phone'
-        value={userInput.phone}
-        onChange={handleChange}
-      />
-      <button
-        className='text-2xl text-red-500 border-4 p-2 bg-stone-300'
-        type='submit'
-      >
-        {' '}
-        Add
-      </button>
-      <ListofUsers user={user} />
-    </form>
+    <>
+      <form
+        className='flex flex-col m-auto w-1/3 gap-y-5'
+        onSubmit={handleSubmit}>
+        <input
+          type='text'
+          placeholder='Add email'
+          className='border-4 p-2 mr-2'
+          name='email'
+          value={userInput.email}
+          onChange={handleChange}
+        />
+        <input
+          type='password'
+          placeholder='Add password'
+          className='border-4 p-2 mr-2'
+          name='password'
+          value={userInput.password}
+          onChange={handleChange}
+        />
+        <input
+          type='text'
+          placeholder='Add phone'
+          className='border-4 p-2 mr-2'
+          name='phone'
+          value={userInput.phone}
+          onChange={handleChange}
+        />
+        <button
+          className='text-2xl text-red-500 border-4 p-2 bg-stone-300'
+          type='submit'>
+          {' '}
+          Add
+        </button>
+      </form>
+      <ListofUsers user={user}  />
+    </>
   );
 };
 
--- src/component/ListofUsers.jsx
+++ src/component/ListofUsers.jsx
@@ -1,12 +1,37 @@
 import React from 'react';
+import style from './index.module.css';
+import edit from '../asset/edit.png';
+import deletei from '../asset/deletei.png';
 
 const ListofUsers = ({ user }) => {
+  console.log('user123:', user);
+
   return (
-    <div>
-      {user.map((item, index) => {
-        <p>{item.email}</p>;
-        <p>{item.phone}</p>;
-      })}
+    <div className={style.table}>
+      <table class='table-fixed min-w-full mx-w-xs '>
+        <thead className='border w-5'>
+          <tr className='border'>
+            <th>Email</th>
+            <th>Phone</th>
+            <th>Edit</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          {user?.map((item, index) => (
+            <tr className='border'>
+              <td class='border w-40 ml-8'>{item.email}</td>
+              <td className='border'>{item.phone}</td>
+              <td className='w-5 '>
+                <img src={edit} alt='' className='w-5' />
+              </td>
+              <td className='w-10'>
+                <img src={deletei} alt='' className='w-5' />
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
